cb20d506236000bc2f93d82476d9bb5aed13c6f7
Fred Souza <fredericoamsouza@gmail.com>
2020-03-23 01:02:45 -0300

    Implementing animated Swiper to insights

--- src/modules/history/screens/InsightsScreen.js
+++ src/modules/history/screens/InsightsScreen.js
@@ -1,63 +1,20 @@
-import React, { useLayoutEffect, useState } from 'react';
-import {
-  RefreshControl,
-  ScrollView,
-  Text,
-  TouchableWithoutFeedback,
-  View,
-} from 'react-native';
+import React, { useLayoutEffect, useRef, useEffect, useState } from 'react';
+import { Text } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
-import { format, isSameMonth } from 'date-fns';
+import SwiperFlatList from 'react-native-swiper-flatlist';
 
 import { useOnMount } from '~shared/hooks';
-import { flatten, makeStyles } from '~shared/styles';
-import { Container, Space } from '~shared/widgets';
 import { withProviders } from '~shared/providers';
 
 import { useLocale } from '~modules/history/intl';
 import { HistoryProvider, useInsights } from '~modules/history/state';
 import { MonthView } from '~modules/history/widgets';
-
-const useStyles = makeStyles(theme => ({
-  container: {
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'flex-start',
-    justifyContent: 'flex-start',
-    flex: 1,
-  },
-  monthsContainer: {
-    borderBottomWidth: 1,
-    borderColor: theme.colors.border,
-    width: theme.maxWidth,
-  },
-  month: {
-    borderLeftWidth: 1,
-    borderRightWidth: 1,
-    borderColor: theme.colors.border,
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    justifyContent: 'center',
-    paddingHorizontal: theme.padding * 2,
-    paddingVertical: theme.padding,
-  },
-  selectedMonth: {
-    backgroundColor: theme.colors.border,
-  },
-  monthTitle: {
-    fontSize: 16,
-  },
-  contentContainer: {
-    flex: 1,
-  },
-}));
+import { format, isThisMonth } from 'date-fns';
 
 export const InsightsScreen = withProviders([HistoryProvider], () => {
-  const styles = useStyles();
   const { getText } = useLocale();
+  const swiperRef = useRef(null);
   const navigation = useNavigation();
-  const [selectedMonth, setSelectedMonth] = useState(new Date());
   const {
     loading,
     error,
@@ -74,31 +31,28 @@ export const InsightsScreen = withProviders([HistoryProvider], () => {
 
   useOnMount(() => getInsights());
 
-  const handleSelectMonth = month => setSelectedMonth(month);
-
-  const statements = getMonthStatements(selectedMonth);
+  useEffect(() => {
+    if (months.length > 0) {
+      const index = months.findIndex(mo => isThisMonth(mo));
+      swiperRef.current.scrollToIndex({ index });
+    }
+  }, [months]);
 
   return (
-    <View style={styles.container}>
-      <View style={styles.monthsContainer}>
-        <ScrollView horizontal showsHorizontalScrollIndicator={false}>
-          {months.map(month => (
-            <TouchableWithoutFeedback
-              key={month.toString()}
-              onPress={() => handleSelectMonth(month)}>
-              <View
-                style={flatten(
-                  styles.month,
-                  isSameMonth(month, selectedMonth) && styles.selectedMonth,
-                )}>
-                <Text style={styles.monthTitle}>{format(month, 'MMM yy')}</Text>
-              </View>
-            </TouchableWithoutFeedback>
-          ))}
-        </ScrollView>
-      </View>
-      {statements.length > 0 && <MonthView statements={statements} />}
-    </View>
+    <SwiperFlatList
+      renderAll
+      ref={swiperRef}
+      data={months}
+      keyExtractor={item => format(item, 'MMyyyy')}
+      renderItem={({ item, index }) => (
+        <MonthView
+          month={item}
+          statements={getMonthStatements(item)}
+          isFirst={index === 0}
+          isLast={index === months.length - 1}
+        />
+      )}
+    />
   );
 });
 
--- src/modules/history/widgets/MonthView.js
+++ src/modules/history/widgets/MonthView.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { ScrollView } from 'react-native';
+import { ScrollView, Text, View } from 'react-native';
+import { format } from 'date-fns';
 
 import { makeStyles } from '~shared/styles';
-import { Container, Space } from '~shared/widgets';
+import { Container, Icon, Space } from '~shared/widgets';
 
 import { PieView } from './PieView';
 import { TopExpensiveStatements } from './TopExpensiveStatements';
@@ -12,6 +13,18 @@ import { TransactionTypeView } from './TransactionTypeView';
 const useStyles = makeStyles(theme => ({
   container: {
     width: theme.maxWidth,
+  },
+  pagination: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    paddingVertical: theme.padding,
+    backgroundColor: theme.colors.border,
+  },
+  monthLabel: {
+    color: theme.colors.text,
+    fontSize: 16,
   },
   contentContainer: {
     paddingVertical: theme.padding * 4,
@@ -22,24 +35,43 @@ const useStyles = makeStyles(theme => ({
   },
 }));
 
-export const MonthView = ({ statements }) => {
+export const MonthView = ({ isFirst, isLast, month, statements }) => {
   const styles = useStyles();
 
   return (
-    <ScrollView
-      style={styles.container}
-      contentContainerStyle={styles.monthViewContainer}>
-      <Container style={styles.contentContainer}>
-        <PieView statements={statements} />
-        <Space height={32} />
-        <TransactionTypeView statements={statements} />
-        <Space height={32} />
-        <TopExpensiveStatements statements={statements} />
-      </Container>
-    </ScrollView>
+    <View style={styles.container}>
+      <View style={styles.pagination}>
+        <View style={styles.leading}>
+          {!isFirst && <Icon name="chevron-left" />}
+        </View>
+        <View style={styles.middle}>
+          <Text style={styles.monthLabel}>{format(month, 'MMM yyyy')}</Text>
+        </View>
+        <View style={styles.trailing}>
+          {!isLast && <Icon name="chevron-right" />}
+        </View>
+      </View>
+      <ScrollView contentContainerStyle={styles.monthViewContainer}>
+        <Container style={styles.contentContainer}>
+          <PieView statements={statements} />
+          <Space height={32} />
+          <TransactionTypeView statements={statements} />
+          <Space height={32} />
+          <TopExpensiveStatements statements={statements} />
+        </Container>
+      </ScrollView>
+    </View>
   );
 };
 
 MonthView.propTypes = {
+  isFirst: PropTypes.bool,
+  isLast: PropTypes.bool,
+  month: PropTypes.object.isRequired,
   statements: PropTypes.arrayOf(PropTypes.any).isRequired,
+};
+
+MonthView.defaultProps = {
+  isFirst: false,
+  isLast: false,
 };
